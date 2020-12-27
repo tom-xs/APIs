9c270019bc34796e0509f0284284c3ac4e04325c
Tomas Xavier Santos <txs@ecomp.poli.br>
2020-12-27 18:35:33 -0300

    primeiro commit

---
+++ YoutubeApi.py
@@ -0,0 +1,10 @@
+from googleapiclient.discovery import build
+import json
+
+SEARCH_INPUT = "voiced out"
+
+youtube = build(serviceName="youtube",version="v3",developerKey="AIzaSyD1HExOrJEk_lPwmOpH9k8fAKrXusQ6KDQ")
+
+reponseDict = youtube.search().list(part="id, snippet",q=SEARCH_INPUT).execute()
+
+print(json.dumps(reponseDict["items"],indent=4))
