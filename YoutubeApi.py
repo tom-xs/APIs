dc409123605a0ec6e05cfecbdaa37889029c061b
Tomas Xavier Santos <txs@ecomp.poli.br>
2020-12-29 01:20:21 -0300

    funcoes auxiliares adicionadas. TODO: 1.selecionar qual video quer baixar\n2.escolher qual cover usar da biblioteca\n3.baixar video e atualizarmetadados

--- YoutubeApi.py
+++ YoutubeApi.py
@@ -1,17 +1,40 @@
 from googleapiclient.discovery import build
 import json
 
-SEARCH_INPUT = "raflum swwan goose"
+SEARCH_INPUT = ""
 
 SERVICE_NAME = "youtube"
 SERVICE_VERSION = "v3"
-DEV_KEY = "AIzaSyD1HExOrJEk_lPwmOpH9k8fAKrXusQ6KDQ"
+DEV_KEY = ""
 
 youtube = build(serviceName= SERVICE_NAME,version=SERVICE_VERSION,developerKey=DEV_KEY)
 
-reponseDict = youtube.search().list(part="id, snippet",q=SEARCH_INPUT).execute()
+def getJsonForInput():
+     responseDict = youtube.search().list(part="id, snippet",q=SEARCH_INPUT).execute()
+     return json.dumps((responseDict["items"]),indent = 4)
 
-print(json.dumps(reponseDict["items"],indent=4))
+def getDicVideosIds(json):
+     listJson = json.split("\n")
+     dicReturn = {}
+
+     for line in listJson:
+          if("videoId" in line):
+               value = "http://youtube.com/watch?v="+line.replace(" ","")[11:-1]
+          if("title" in line):
+               key = line[22:-2]
+               dicReturn[key]=value
+     
+     return dicReturn
+
+def printDicElements(dic):
+     for k,v in dic.items():
+          print(k, ":", v)
+
+dic = getDicVideosIds(getJsonForInput())
+
+printDicElements(dic)
+
+
 
 """
 TODO:1-checar se a musica é a que a pessoa procura  
