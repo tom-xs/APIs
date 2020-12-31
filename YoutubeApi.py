218197cd4542c85cc985fe01ecf17e29c798b25f
Tomas Xavier Santos <txs@ecomp.poli.br>
2020-12-31 01:19:05 -0300

    TODO: search song library for metadada

--- YoutubeApi.py
+++ YoutubeApi.py
@@ -1,38 +1,51 @@
 from googleapiclient.discovery import build
+from pytube import YouTube as yt
 import json
 
-SEARCH_INPUT = ""
+SEARCH_INPUT = "swan goose raflum"
 
 SERVICE_NAME = "youtube"
 SERVICE_VERSION = "v3"
-DEV_KEY = ""
+DEV_KEY = "AIzaSyD1HExOrJEk_lPwmOpH9k8fAKrXusQ6KDQ"
 
-youtube = build(serviceName= SERVICE_NAME,version=SERVICE_VERSION,developerKey=DEV_KEY)
+youtube = build(serviceName=SERVICE_NAME,version=SERVICE_VERSION,developerKey=DEV_KEY)
 
 def getJsonForInput():
      responseDict = youtube.search().list(part="id, snippet",q=SEARCH_INPUT).execute()
      return json.dumps((responseDict["items"]),indent = 4)
 
-def getDicVideosIds(json):
+def getListsVideosIds(json):
      listJson = json.split("\n")
-     dicReturn = {}
+     listNames = []
+     listURLS = []
 
      for line in listJson:
           if("videoId" in line):
-               value = "http://youtube.com/watch?v="+line.replace(" ","")[11:-1]
+               listURLS.append("http://youtube.com/watch?v="+line.replace(" ","")[11:-1])
           if("title" in line):
-               key = line[22:-2]
-               dicReturn[key]=value
+               listNames.append(line[22:-2])
      
-     return dicReturn
+     return listNames, listURLS
 
-def printDicElements(dic):
-     for k,v in dic.items():
-          print(k, ":", v)
+def downloadaVideo(url):
+     print("download iniciado")
+     video = yt(url)
+     video.streams.filter(only_audio=True)[0].download()
+     print("download finalizado")
 
-dic = getDicVideosIds(getJsonForInput())
+def printElements(listNames,listURLS):
+     for i in range(0,len(listNames)):
+          print(str(i) + " - "+ listNames[i] + " : " + listURLS[i])
 
-printDicElements(dic)
+
+def main():
+     listaNames, listaURLs = getListsVideosIds(getJsonForInput())
+     printElements(listaNames,listaURLs)
+     downloadaVideo(listaURLs[0])
+
+
+if __name__ == "__main__":
+     main()
 
 
 
